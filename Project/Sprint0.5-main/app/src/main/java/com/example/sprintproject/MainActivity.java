bd5bd9ddb34a985165391f1f3c0681d6963a6400
ShreyaPrakash12 <sprakash92@gatech.edu>
2025-10-04 12:48:13 -0400

    Created Splash, Savings Circles, Chatbot screens

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.sprintproject;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.sprintproject.R;
+import com.example.sprintproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startBtn;
+    private Button quitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        startBtn = findViewById(R.id.startBtn);
+        quitBtn = findViewById(R.id.quitBtn);
     }
+
+
 }

0e58f19c3f89553c91b68169423296c30ef15cd0
austinchan12 <austinchan0812@gmail.com>
2025-10-05 22:41:57 -0400

    fixed app build errors

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -28,22 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         startBtn = findViewById(R.id.startBtn);
         quitBtn = findViewById(R.id.quitBtn);
-
-        Button expenseLogButton = findViewById(R.id.btn_expense_log);
-        expenseLogButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ExpenseLogActivity.class));
-            }
-        });
-
-        Button budgetsButton = findViewById(R.id.btn_budgets);
-        budgetsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, BudgetsActivity.class));
-            }
-        });
     }
 
 

40838bf34a00aab4132c14d50e0723ea81905ae6
Nehal70 <nehalsinghal06@gmail.com>
2025-10-16 14:22:09 -0400

    Login Activity Fix
    Fixed MainActivity and LoginActivity so that start button routes to login activity
    
    Also fixed mainactivity's syntax errors.

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
@@ -47,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         auth.signInWithEmailAndPassword(email, password)
                 .addOnSuccessListener(authResult -> {
                     Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(this, MainActivity.class));
+                    startActivity(new Intent(this, DashboardActivity.class));
                     finish();
                 })
                 .addOnFailureListener(e ->
